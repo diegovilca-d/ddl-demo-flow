de6aed4826b22d38173154bec4abe818fdfc71fe
Diego Percy Vilca Daza <diego.vilca@rimac.com.pe>
2025-05-28 10:27:12 -0500

    feat: sol2

--- ddls/raw/tmp_prueba_github/sample_table/schema.sql
+++ ddls/raw/tmp_prueba_github/sample_table/schema.sql
@@ -2,5 +2,8 @@ CREATE OR REPLACE TABLE `tmp_prueba_github.sample_table` (
     id INT64 NOT NULL,
     name STRING,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    is_active BOOL
+    is_active BOOL,
+    registered_by STRING,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    updated_by STRING,
 );
